e2734a43e0c175a88cfb1eed503ac8a12107c02e
nitinbadgu <nitinbadgu5@gmail.com>
2021-09-06 12:18:49 +0530

    Latest changes

---
+++ src/app/profile/profile-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DisplayProfileComponent } from './components/display/display-profile/display-profile.component';
+import { DisplayProfilesComponent } from './components/display/display-profiles/display-profiles.component';
+import { AddEduComponent } from './components/exp/add-edu/add-edu.component';
+import { AddExpComponent } from './components/exp/add-exp/add-exp.component';
+import { CreateProfileComponent } from './components/profile/create-profile/create-profile.component';
+
+const routes: Routes = [
+  {
+    path: 'create-profile',
+    component: CreateProfileComponent,
+  },
+  {
+    path: 'display-profile/:id',
+    component: DisplayProfileComponent,
+  },
+
+  {
+    path: 'display-profiles',
+    component: DisplayProfilesComponent,
+  },
+
+  {
+    path: 'add-exp',
+    component: AddExpComponent,
+  },
+  {
+    path: 'add-edu',
+    component: AddEduComponent,
+  },
+  {
+    path: 'display-profile/:id',
+    component: DisplayProfileComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ProfileRoutingModule {}
